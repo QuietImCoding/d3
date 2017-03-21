3c3e384f8b494387fbf497214eeacaf823bdec80
Daniel Monteagudo <dmonte3210@gmail.com>
2017-03-20 21:06:01 -0400

    some function

--- datas.js
+++ datas.js
@@ -1,15 +1,72 @@
-var presdata = [[2.99, 3.54, -552, -3.3, 16.5], [2.55, 3.02, -464, -3.1, 14.9]];
+var presdata = [[2.99, 3.54, -552, -3.3, 16.5], [2.55, 3.02, -464, -3.1, 14.9], [2.49, 2.2, 290, 2.3, 12.5], [1.55, 1.97, -413, -4.5, 9.15]];
+var diagramdata = [[2.99, 3.54, -.552, -3.3, 16.5], [2.55, 3.02, -.464, -3.1, 14.9], [2.49, 2.2, .290, 2.3, 12.5], [1.55, 1.97, -.413, -4.5, 9.15]];
+var svg;
+var height, width;
 
 var showdata = function(d, i) {
-    thing = d3.select("#tabledata");
-    thing.data(presdata[i]);
-    thing.enter();
-    thing.append("td").text
-    console.log(i);
+    updateTable(i);
+    updateDiagram(i);
+};
+
+var updateDiagram = function(i) {
+    var circle = svg.selectAll("circle");
+    if (circle.size()==0) {
+	circle.data(diagramdata[i])
+	    .enter()
+	    .append("svg:circle")
+	    .attr("cx", function(d, i) {
+		return width/5 * i;
+	    })
+	    .attr("cy", 250)
+	    .attr("r", function(d) { return Math.abs(d) * 10; })
+	    .attr("stroke", function(d, i) { return "#FFFFFF";})
+	    .attr("fill", function(d) {
+		if (d > 0) {
+		    return "#FF0000"
+		} else {
+		    return "#000000"
+		}
+	    });
+    } else {
+	circle.data(diagramdata[i])
+	.attr("cx", function(d, i) {
+		return width/5 * i;
+	    })
+	    .attr("cy", 250)
+	    .attr("r", function(d) { return Math.abs(d) * 10; })
+	    .attr("stroke", function(d, i) { return "#FFFFFF";})
+	    .attr("fill", function(d) {
+		if (d > 0) {
+		    return "#FF0000"
+		} else {
+		    return "#000000"
+		}
+	    });
+    }
+};
+
+var updateTable = function(i) {
+    var row = d3.select("#tabledata").selectAll("td");
+    if (row.size()==0) {
+	row.data(presdata[i])
+	    .enter()
+	    .append("td")
+	    .html( function(d) {
+		return d;
+	    });
+    } else {
+	row.data(presdata[i])
+	    .html( function(d) {
+		return d;
+	    });
+    }
 };
 
 var setup = function() {
     d3.selectAll("img").on("click", showdata);
+    svg = d3.select("#diagram");
+    width = svg.node().getBoundingClientRect().width;
+    height = svg.node().getBoundingClientRect().height;
 }
 
 window.onload = setup;
